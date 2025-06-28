f21e992bb544ef4e445ce0a5a606469b1523287a
Your Name <you@example.com>
2025-06-28 15:03:10 -0300

    Last update

--- Part2/introdemo2/eslint.config.js
+++ Part2/introdemo2/eslint.config.js
@@ -1,33 +1,55 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+// eslint.config.js
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
-  { ignores: ['dist'] },
+  {
+    ignores: ['dist', 'node_modules'],
+  },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
       },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      ...jsxA11y.configs.recommended.rules,
+
+      'react/prop-types': 'off',
+      'no-console': 'off',
+      'indent': ['error', 2],
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'arrow-parens': ['error', 'always'],
+      'no-unused-vars': ['warn', { 'argsIgnorePattern': '^_', 'varsIgnorePattern': '^[A-Z_]' }],
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+    settings: {
+      react: {
+        version: 'detect', // This is crucial to prevent "React version not specified" warnings
+      },
     },
   },
-]
+];
